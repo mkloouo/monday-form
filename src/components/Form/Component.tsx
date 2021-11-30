eaa42c5e9b7b9af1be78fe67edab65c2d4db867f
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-30 14:08:18 +0200

    replace default setup with custom

---
+++ src/components/Form/Component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface FormProps {}
+
+export const Form = ({}: FormProps) => {
+  return (
+    <div>
+      <p>some text</p>
+    </div>
+  );
+};
+
+Form.displayName = 'Form';
